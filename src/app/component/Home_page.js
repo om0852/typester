a00ea80f230e9b90d42e0163f42df8b803aef525
omsalunke0852 <103877025+om0852@users.noreply.github.com>
2024-02-29 22:22:00 +0530

    implementing socket for trial basics

--- src/app/component/Home_page.js
+++ src/app/component/Home_page.js
@@ -3,7 +3,18 @@ import "../../../sass/component/home.scss";
 import { useState, useEffect } from "react";
 import Cookie from "js-cookie"
 import Link from "next/link";
+import { io } from "socket.io-client";
 export default function Home_Page() {
+    const [socket, setSocket] = useState(0);
+    const handleSend = () => {
+    }
+    useEffect(() => {
+        const socket = io("http://localhost:3001")
+        setSocket(socket);
+        socket.emit("message", "omsalunke")
+
+        handleSend();
+    }, [])
     const [bgColor, setbgColor] = useState("black");
     const [color, setColor] = useState("white");
     useEffect(() => {
@@ -26,6 +37,20 @@ export default function Home_Page() {
 
             <div className="card-container" style={{ background: color }}>
 
+                <div className="card">
+                    <h2 className="title">TYPING SPEED TEST</h2>
+                    <p>Test Your Typing Skill</p>
+                    <select onChange={handleTime}>
+                        <option value={1}>1min</option>
+                        <option value={2}>2min</option>
+                        <option value={5}>5min</option>
+                        <option value={10}>10min</option>
+                        <option value={20}>20min</option>
+                    </select>
+
+                    <Link className="start_btn" href={"/timertesting"}>Start</Link>
+                    <Link className="start_btn voilet" href={"/history"}>History</Link>
+                </div>
                 <div className="card">
                     <h2 className="title">TYPING SPEED TEST</h2>
                     <p>Test Your Typing Skill</p>
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -13,7 +13,44 @@ export default function Profile() {
                         <div className="profile-editbutton"><button>Edit Profile</button></div>
                     </div>
                     <div className="profile-result-container">
+                        <div className="latest-result-section">
+                            <div className="result-title">Last 3 Matches</div>
+                            <div className="result-card-container">
+                                <div className="result-card">
+                                    <p>win
+                                        Accuraccy</p>
+                                    <p>
+                                        playername
+                                    </p>
 
+                                </div>
+                                <div className="result-card">
+                                    <p>win
+                                        Accuraccy</p>
+                                    <p>
+                                        playername
+                                    </p>
+
+                                </div>
+                                <div className="result-card">
+                                    <p>win
+                                        Accuraccy</p>
+                                    <p>
+                                        playername
+                                    </p>
+
+                                </div>
+                                <div className="result-card">
+                                    <p>win
+                                        Accuraccy</p>
+                                    <p>
+                                        playername
+                                    </p>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div className="profile-calender"></div>
                     </div>
                 </div>
 
